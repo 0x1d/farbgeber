473a680669a315a32f9667c510de9d3dde7fb479
coon@c-base.org <coon@defekt.localdomain>
2017-02-07 19:37:47 +0100

    use binary format on test client

--- mqtt_test_client.py
+++ mqtt_test_client.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import struct 
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, rc):
@@ -10,7 +11,16 @@ def on_connect(client, userdata, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    data = struct.Struct('7s B BBB BBB BBB BBB BBB BBB').unpack(msg.payload)
+
+    print("Topic: " + msg.topic)
+    print("Base color: #%02X%02X%02X" % (data[2],  data[3],  data[4])) 
+    print("Variant 1:  #%02X%02X%02X" % (data[5],  data[6],  data[7]))
+    print("Variant 2:  #%02X%02X%02X" % (data[8],  data[9],  data[10]))
+    print("Variant 3:  #%02X%02X%02X" % (data[11], data[12], data[13]))
+    print("Variant 4:  #%02X%02X%02X" % (data[14], data[15], data[16]))
+    print("Contrast:   #%02X%02X%02X" % (data[17], data[18], data[19]))
+    print("")
 
 client = mqtt.Client()
 client.on_connect = on_connect
