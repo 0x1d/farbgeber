5e68b63accf16f5517638aece547a871b682f26e
coon@c-base.org <coon@defekt.localdomain>
2017-05-06 22:26:36 +0200

    lay out config to config.h

--- esp8266/mqtt_client/mqtt_client.ino
+++ esp8266/mqtt_client/mqtt_client.ino
@@ -1,51 +1,35 @@
 /*
- Basic ESP8266 MQTT example
+ ESP8266 Farbgeber client
 
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
+ Author: coon@c-base.org
+ Last Maintenance: 06. May 2017
+ 
+ Description: Connects to a WIFI and connectcs to a MQTT server and acts as 
+ a client for the farbgeber.
 
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,<fC
-    else switch it off
-
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
-
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
-
+ - Modify config.h first but do not check it in into github!
 */
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <Adafruit_NeoPixel.h>
 #include <ArduinoJson.h>
+#include "config.h"
 
-#define PIN D8
-#define NUM_LEDS 60 // 150
-#define NUM_GROUPS 6 // base + variant1,2,3,4 + contrast
 #define LEDS_PER_GROUP NUM_LEDS / NUM_GROUPS
 
-// Update these with values suitable for your network.
-
-const char* ssid = "<enter ssid here>";
-const char* password = "<enter password here>";
-const char* mqtt_server = "c-beam.cbrp3.c-base.org";
+// Modify the following variables in config.h:
+// ---------------------------------------------
+extern const char* wifi_ssid;
+extern const char* wifi_password;
+extern const char* mqtt_server;
+extern const char* mqtt_topic;
+// ---------------------------------------------
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, DATA_PIN, NEO_GRB + NEO_KHZ800);
 
-long lastMsg = 0;
-char msg[50];
-int value = 0;
 
 typedef struct Color {
   unsigned char red;
@@ -53,16 +37,9 @@ typedef struct Color {
   unsigned char blue;
 };
 
-typedef struct ColorElements {
-  char pBinary[8];
-  Color base;
-  Color variant[4];
-  Color contrast;
-};
-
 void setLedGroupColor(int group, Color color) {
   int startLed = group * LEDS_PER_GROUP;
-  int brightnessDivider = 1; // 32
+  int brightnessDivider = 1;
 
   color.red   /= brightnessDivider;
   color.green /= brightnessDivider;
@@ -89,21 +66,39 @@ void setup() {
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
     
-  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  pinMode(BUILTIN_LED, OUTPUT);
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+
+  long lastMsg = 0;
+  char msg[50];
+  int value = 0;
+
+  while(true) {
+    if (!client.connected())
+      reconnect();
+  
+    client.loop();
+
+    long now = millis();
+    if (now - lastMsg > 2000) {
+      lastMsg = now;
+      ++value;
+      snprintf (msg, 75, "Heartbeat #%ld", value);
+      Serial.println(msg);
+    }
+  }
 }
 
 void setup_wifi() {
   delay(10);
-  // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
-  Serial.println(ssid);
+  Serial.println(wifi_ssid);
 
-  WiFi.begin(ssid, password);
+  WiFi.begin(wifi_ssid, wifi_password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -168,7 +163,7 @@ void reconnect() {
     
     if (client.connect("farbgeber_client")) {
       Serial.println("connected");      
-      client.subscribe("panel/weltenbaulab");
+      client.subscribe(mqtt_topic);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -179,16 +174,5 @@ void reconnect() {
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-
-  long now = millis();
-  if (now - lastMsg > 2000) {
-    lastMsg = now;
-    ++value;
-    snprintf (msg, 75, "Heartbeat #%ld", value);
-    Serial.println(msg);
-  }
+  // DO NOT USE
 }
