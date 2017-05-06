d6bff5250a3245c85d75cb50927f8d79bbf00fb6
coon@c-base.org <coon@defekt.localdomain>
2017-05-07 00:04:08 +0200

    color_circle: changed... something

--- color_circle.py
+++ color_circle.py
@@ -6,10 +6,10 @@ from colour import Color
 from Tkinter import *
 import threading
 
-def draw_color_circle(time_value, base_color, canvas=0, width=0, height=0):
+def draw_line(index, base_color, canvas=0, width=0, height=0):
     w = width / 12.0
     x = width / 2 - w / 2
-    y = int(time_value / 6)
+    y = int(index)
 
     canvas.create_line(x + 0 * width, y, x + w, y, fill=base_color.hex)
     canvas.update()
@@ -23,10 +23,15 @@ if __name__ == "__main__":
     canvas.pack()
 
     time_value = 0.0
-    while(time_value < 3600):
-        palette = farbgeber.generate_palette(time_value = time_value)
-        draw_color_circle(time_value, palette['base_color'], canvas, canvas_width, canvas_height)
+    color_offset = 0.0
+    while(time_value + color_offset < 3600):
+        index = time_value / 6
+        palette = farbgeber.generate_palette(time_value = time_value + color_offset)
+        draw_line(index, palette['base_color'], canvas, canvas_width, canvas_height)
         time_value += 6
+
+#        if time_value == 600:
+#            color_offset += 600
 
     mainloop()
 
